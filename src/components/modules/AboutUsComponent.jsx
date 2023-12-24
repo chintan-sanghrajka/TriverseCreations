02da2c2938111b4d07db6ae5f1c0612d8757e0d5
ChintanKS <chintansanghrajka2509@gmail.com>
2023-12-25 01:21:36 +0530

    Make UI related changes

--- src/components/modules/AboutUsComponent.jsx
+++ src/components/modules/AboutUsComponent.jsx
@@ -3,11 +3,13 @@ import CountUp from 'react-countup'
 import ScrollTrigger from 'react-scroll-trigger'
 
 const AboutUsComponent = () => {
+    console.log()
+
     const [counterOn, setCounterOn] = useState(false)
     return (
         <div>
             <div className='container mt-5'>
-                <p className='about_para'>Founded in 2018, based in Mumbai with 2 printers bed side as a interesting concept to study, ISAGON now stands with a 3D Printing farm with top notch service providers to clients nation wide and holds a Forte in Corporate Gifting Solutions.</p>
+                <p className='about_para'>Welcome to Triverse Creations, a Mumbai-based 3D printing venture established on November 11, 2023. Committed to delivering exceptional services, we operate with a fleet of 20+ advanced printers, ensuring precision and innovation in every project. Our focus on sustainability sets us apart, minimizing waste while maximizing efficiency. Beyond quality and competitive pricing, we aspire to expand our capabilities by growing our machine fleet. Join us on this transformative journey, where each creation reflects our passion, precision, and commitment to excellence.</p>
                 <div className='row'>
                     <div className='col-md-4 col-12 py-md-5 py-2 numbers_div'>
                         <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
--- src/components/pages/AboutUsPage.jsx
+++ src/components/pages/AboutUsPage.jsx
@@ -7,6 +7,7 @@ const AboutUsPage = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
     return (
         <>
             <IntroComponent head={"Triverse Creations"} para={"Welcome to the World of 3D Printing"} imageClass={"image_container_about"} />
